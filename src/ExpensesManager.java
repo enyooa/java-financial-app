ee777a999bfb73ebd16a81edeff77542f4fde177
Assylbek Akpanov <asilbekk87@gmail.com>
2023-10-27 15:42:18 +0600

    test commit

---
+++ src/ExpensesManager.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ExpensesManager {
+    public double saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses) {
+
+        System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+        int day = scanner.nextInt();
+        System.out.println("Введите размер траты:");
+        double expense = scanner.nextDouble();
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в тенге: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 5000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    public void printAllExpenses(double[] expenses) {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " тенге");
+        }
+    }
+
+    public double findMaxExpense(double[] expenses) {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+}
